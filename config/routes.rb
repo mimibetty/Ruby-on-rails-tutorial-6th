822c79a15f6db251c37161cd92500dff7a9756fa
mimibetty <minh74dn@gmail.com>
2024-09-10 13:03:11 +0700

    rails generate scaffold User name:string email:string

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  # normal route for user format RESTFUL
+  resources :users
+
+  # resources :users, except: [:edit] do
+  #   member do
+  #     get 'updatefunc', action: :edit
+  #   end
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'users#index'
   get '/goodbye', to: 'convert_application#goodbye'
 end
