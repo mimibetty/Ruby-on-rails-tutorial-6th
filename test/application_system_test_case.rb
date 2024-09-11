5ff10c77071da98afeab6c462394010a17df127a
mimibetty <minh74dn@gmail.com>
2024-09-11 08:59:47 +0700

    advanced testing setup, but in guard test, there are some troubles because of unmatched version of chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,8 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # driven_by :selenium, using: :headless_firefox, screen_size: [1400, 1400]
+  setup do
+    Webdrivers::Chromedriver.required_version = "128.0.6613.137"
+  end
 end
