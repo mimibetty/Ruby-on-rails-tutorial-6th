e25c508635bed3a15efcd3ab08f753d844e7a871
mimibetty <minh74dn@gmail.com>
2024-09-16 16:11:32 +0700

    done chap 5

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,31 +8,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get root' do
-    get root_url
+    get root_path
     assert_response :success
   end
   test 'should get home' do
-    get static_pages_home_url
+    get home_path
     assert_response :success
     # assert_select "title", "Home | #{@base_title}"
     assert_select 'title', 'Ruby on Rails Tutorial Sample App'
   end
 
   test 'should get help' do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select 'title', "Help | #{@base_title}"
   end
 
   test 'should get about' do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select 'title', "About | #{@base_title}"
   end
 
   test 'should get contact' do
-    get static_pages_about_url
+    get contact_path
     assert_response :success
-    assert_select 'title', "About | #{@base_title}"
-  end
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    end
 end
