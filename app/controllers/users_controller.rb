e694405bd937ec3963d6f1270e5c51fef3214b3a
mimibetty <minh74dn@gmail.com>
2024-09-18 17:24:39 +0700

    done chap 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,15 +27,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    # respond_to do |format|
+    if @user.save
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
+      # format.html { redirect_to user_url(@user), notice: 'User was successfully created.' }
+      # format.json { render :show, status: :created, location: @user }
+    else
+      render 'new'
+      # format.html { render :new, status: :unprocessable_entity }
+      # format.json { render json: @user.errors, status: :unprocessable_entity }
     end
+    # end
   end
 
   # PATCH/PUT /users/1 or /users/1.json
@@ -70,6 +73,7 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password,
+    :password_confirmation)
   end
 end
