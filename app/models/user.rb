7740aea5cd650161db6a8924fce831fdec7ef878
mimibetty <minh74dn@gmail.com>
2024-09-24 14:59:12 +0700

    done 11.2.3 and fix minitest-reporter, minitest gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
+
   has_many :microposts
   validates :name, presence: true, length: {maximum: 50}
 
@@ -41,5 +43,15 @@ class User < ApplicationRecord
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_24_031712) do
+ActiveRecord::Schema.define(version: 2024_09_24_043530) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 2024_09_24_031712) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
